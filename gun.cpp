a0ffd8688402291267094d30efccb02ae2abc718
Spencer Lamoreaux <lam21016@byui.edu>
2024-12-11 19:30:41 -0700

    gun

--- gun.cpp
+++ gun.cpp
@@ -98,16 +98,16 @@ void drawRectangle(const Position& pt,
   * GUN : DISPLAY
   * Display the gun on the screen
   *********************************************/
-void RealGun::display() const
+void Gun::display() const
 {
    drawRectangle(pt, M_PI_2 - angle, 10.0, 100.0, 1.0, 1.0, 1.0);
 }
 
 /*********************************************
- * RealGun : INTERACT
- * Move the RealGun
+ * Gun : INTERACT
+ * Move the Gun
  *********************************************/
-void RealGun::interact(int clockwise, int counterclockwise)
+void Gun::interact(int clockwise, int counterclockwise)
 {
    // move it
    if (clockwise > 0)
